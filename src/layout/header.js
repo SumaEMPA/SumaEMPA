dd18a30cb2051dc20bbfcfeba829a84803566843
Lucas <lucashpandolfo@gmail.com>
2021-03-01 14:19:51 -0300

    Cambios de estética Fuentes y logo nuevo

--- src/layout/header.js
+++ src/layout/header.js
@@ -6,7 +6,6 @@ const Header = {
                  m("nav",
                    m("logo",
                      m("img", {src: require("/img/logo.svg"), onclick: () => m.route.set("/")})),
-                   m("title", "SUMA EMPA"),
                    m("search", m("img", {src: require("/img/search.svg"), onclick: () => m.route.set("/material")})),
                    m("hamburguer", m("img", {src: require("/img/hamburguer.svg")}))));
     }
