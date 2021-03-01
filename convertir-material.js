fa3d74239eccd617e2bf7aa0ad0c542aac2ecaca
Lucas <lucashpandolfo@gmail.com>
2021-02-28 22:12:02 -0300

    Agrego funcionalidad para poder parsear subdirectorios

--- convertir-material.js
+++ convertir-material.js
@@ -2,24 +2,30 @@ const path = require('path');
 const fs = require('fs');
 const YAML = require('yaml');
 
-const directoryPath = path.join(__dirname, 'material');
 
 var database = [];
 
-fs.readdir(directoryPath, function (err, files) {
-
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    }
+function processDirectory(dirPath){
+    files = fs.readdirSync(dirPath);
 
     files.forEach(function (file) {
-        console.log("File", directoryPath, file);
-        const yaml = fs.readFileSync(path.join(directoryPath, file), 'utf8')
-        const parsed = YAML.parse(yaml)
-        database.push(parsed);
+
+        const filePath = path.join(dirPath, file);
+
+        console.log("File", filePath);
+        if(fs.lstatSync(filePath).isDirectory()){
+            processDirectory(filePath);
+        }
+        else{
+            const yaml = fs.readFileSync(filePath, 'utf8')
+            const parsed = YAML.parse(yaml)
+            database.push(parsed);
+        }
     });
 
     console.log(database);
-    fs.writeFile(path.join(__dirname, "dist", "material.json"), JSON.stringify(database), 'utf-8', ()=>console.log("ok"));
-});
+}
 
+
+processDirectory(path.join(__dirname, 'material'));
+fs.writeFile(path.join(__dirname, "dist", "material.json"), JSON.stringify(database), 'utf-8', ()=>console.log("ok"));
