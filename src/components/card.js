f9d013e01608965cbe8a1c9727236a2244cf3ecd
Lucas <lucashpandolfo@gmail.com>
2021-03-01 21:59:39 -0300

    Modifico las tarjetas para que se muestren alienadas con texto

--- src/components/card.js
+++ src/components/card.js
@@ -8,14 +8,18 @@ const Card = {
         const links = attrs.links || [];
 
         return m("card.fadein",
-                 m("image",
+                 m("card-image",
                    m("img", {src: attrs.image})),
-                 m("content",
-                   m("h3", attrs.title),
-                   m("p", attrs.content),
-                   links.length && content.length?m("hr"):null,
-                   m("links",
-                     links.map(link => m("a.btn", {href: link.url, target: link.newWindow?"_blank":""}, link.name)))));
+                 m("card-content",
+
+                   m("card-body",
+                     m("h3", attrs.title),
+                     m("p", attrs.content)),
+
+                   m("card-footer",
+                     links.length && content.length?m("hr"):null,
+                     m("links",
+                       links.map(link => m("a.btn", {href: link.url, target: link.newWindow?"_blank":""}, link.name))))));
     }
 }
 
