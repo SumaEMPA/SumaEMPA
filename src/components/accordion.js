1a128eb7a1ed1df7c949c3c26e3cdc5693373f12
Lucas <lucashpandolfo@gmail.com>
2021-03-13 22:18:06 -0300

    Acordiones independientes

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -1,34 +1,38 @@
 const m = require("mithril");
 
 function groupByTwo(array){
-    var groups = [];
+  var groups = [];
 
-    for(var i = 0; i < array.length; i += 2)
-        groups.push(array.slice(i, i + 2));
+  for(var i = 0; i < array.length; i += 2)
+    groups.push(array.slice(i, i + 2));
 
-    return groups;
+  return groups;
+}
+
+const AccordionItem = {
+  open: false,
+  view(vnode){
+    return m(".accordion__item",
+             m(".accordion__chevron", {
+               class: vnode.state.open && "accordion__chevron--closed"
+             }, "❯"),
+             m(".accordion__title", {
+               onclick: () => vnode.state.open = !vnode.state.open,
+               class: vnode.state.open?null:"accordion__title--closed"
+             }, vnode.attrs.title),
+             m(".accordion__body-container", {class: vnode.state.open?"accordion__body-container--show":"accordion__body-container--hide"},
+               m(".accordion__body", {class: open?"accordion__body--show":"accordion__body--hide"},
+                 vnode.state.open && vnode.children)))
+  }
 }
 
 const Accordion = {
-    selected: -1,
-    view(vnode){
-        const children = groupByTwo(vnode.children || []);
+  view(vnode){
+    const children = groupByTwo(vnode.children || []);
 
-        return m(".accordion",
-                 children.map(([title, body], i) =>{
-                     let open = (i==this.selected);
-                     return m(".accordion__item",
-                              m(".accordion__chevron", {class: open?null:"accordion__chevron--closed"},
-                                "❯"),
-                              m(".accordion__title", {
-                                onclick: () => this.selected = open?-1:i,
-                                class: open?null:"accordion__title--closed"
-                              }, title),
-                              m(".accordion__body-container", {class: open?"accordion__body-container--show":"accordion__body-container--hide"},
-                                m(".accordion__body", {class: open?"accordion__body--show":"accordion__body--hide"},
-                                  open?body:null)))
-                 }));
-    }
+    return m(".accordion",
+             children.map(([title, body], i) =>  m(AccordionItem, {title}, body)))
+  }
 }
 
 
